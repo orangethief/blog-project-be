bb6d5922a473cde20e81d5db0127b69f52843d0e
Lisa <helbig-lisa@web.de>
2024-10-26 13:37:36 +0200

    correcting url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use('/posts', postRouter);
+app.use('/api/v1/posts', postRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
